8574028d18b891592b8c5248f30ce672af4c5e75
eaakre <58789308+eaakre@users.noreply.github.com>
2023-04-21 08:44:11 -0500

    Add files via upload

---
+++ hurdlers.js
@@ -0,0 +1,136 @@
+const hurdlers = {
+    boys: [
+        {
+            first: "Jack",
+            last: "Guscette",
+            grade: 10,
+        },
+        {
+            first: "Desire",
+            last: "Masiri",
+            grade: 10,
+        },
+        {
+            first: "Joaquin",
+            last: "Valdez",
+            grade: 8,
+        },
+        {
+            first: "Elijah",
+            last: "Morris",
+            grade: 9,
+        },
+        {
+            first: "Bradyn",
+            last: "Stockert",
+            grade: 11,
+        },
+        {
+            first: "Zander",
+            last: "Senger",
+            grade: 9,
+        },
+        {
+            first: "Rory",
+            last: "Senger",
+            grade: 9,
+        },
+        {
+            first: "Ahmed",
+            last: "Al-Zenad",
+            grade: 11,
+        },
+        {
+            first: "Cedric",
+            last: "Patterson",
+            grade: 9,
+        },
+        {
+            first: "Mag",
+            last: "Cummings",
+            grade: 10,
+        },
+        {
+            first: "Terrance",
+            last: "Booth",
+            grade: 9,
+        },
+    ],
+    girls: [
+        {
+            first: "Samantha",
+            last: "Gustafson",
+            grade: 12,
+        },
+        {
+            first: "Brooke",
+            last: "Finn",
+            grade: 9,
+        },
+        {
+            first: "Emma",
+            last: "Esposito",
+            grade: 9,
+        },
+        {
+            first: "Soludo",
+            last: "Obi",
+            grade: 9,
+        },
+        {
+            first: "McKenna",
+            last: "Gross",
+            grade: 9,
+        },
+        {
+            first: "Ariana",
+            last: "Gardner",
+            grade: 10,
+        },
+        {
+            first: "Lyla",
+            last: "Fiechtner",
+            grade: 10,
+        },
+        {
+            first: "Michelle",
+            last: "Gee",
+            grade: 9,
+        },
+        {
+            first: "Kadi",
+            last: "Sacko",
+            grade: 11,
+        },
+        {
+            first: "Ejatu",
+            last: "Bah",
+            grade: 10,
+        },
+        {
+            first: "Rylee",
+            last: "Larson",
+            grade: 10,
+        },
+        {
+            first: "Lucia",
+            last: "Atendaye",
+            grade: 11,
+        },
+        {
+            first: "Katelyn",
+            last: "Lowenberg",
+            grade: 9,
+        },
+        {
+            first: "Harnoor",
+            last: "Johal",
+            grade: 9,
+        },
+        {
+            first: "Berthina",
+            last: "Caesar",
+            grade: 10,
+        },
+    ]
+}
