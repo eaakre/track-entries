d0d757f133b4ca1badf0e970bb157de43bac2aaa
eaakre <58789308+eaakre@users.noreply.github.com>
2023-05-11 14:01:11 -0500

    distance events

--- distanceEvents.js
+++ distanceEvents.js
@@ -2,5 +2,5 @@ boys400d.appendChild(athleteCheckbox(distance.boys, boys400dList, '400'));
 boys800.appendChild(athleteCheckbox(distance.boys, boys800List, '800'));
 boys1600.appendChild(athleteCheckbox(distance.boys, boys1600List, '1600'));
 boys3200.appendChild(athleteCheckbox(distance.boys, boys3200List, '3200'));
-boys4400d.appendChild(athleteCheckbox(distance.boys, boys4400dList, '4400d'));
-boys4800.appendChild(athleteCheckbox(distance.boys, boys4800List, '4800'));
+boys4x4d.appendChild(athleteCheckbox(distance.boys, boys4400dList, '4400d'));
+boys4x8.appendChild(athleteCheckbox(distance.boys, boys4800List, '4800'));
