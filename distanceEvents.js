4d3a4526384cd2a06f1f98e90f52d334f4fd41d6
eaakre <58789308+eaakre@users.noreply.github.com>
2023-05-11 12:45:59 -0500

    event js

--- distanceEvents.js
+++ distanceEvents.js
@@ -1,6 +1,6 @@
-boys400d.appendChild(athleteCheckbox(sprinters.boys, boys100List, '400'));
-boys800.appendChild(athleteCheckbox(sprinters.boys, boys100List, '800'));
-boys1600.appendChild(athleteCheckbox(sprinters.boys, boys100List, '1600'));
+boys400d.appendChild(athleteCheckbox(distance.boys, boys400dList, '400'));
+boys800.appendChild(athleteCheckbox(distance.boys, boys800List, '800'));
+boys1600.appendChild(athleteCheckbox(distance.boys, boys1600List, '1600'));
 boys3200.appendChild(athleteCheckbox(distance.boys, boys3200List, '3200'));
-boys4x4.appendChild(athleteCheckbox(sprinters.boys, boys100List, '4400'));
-boys4x8.appendChild(athleteCheckbox(sprinters.boys, boys100List, '4800'));
+boys4x4.appendChild(athleteCheckbox(distance.boys, boys4400dList, '4400'));
+boys4x8.appendChild(athleteCheckbox(distance.boys, boys4800List, '4800'));
---
+++ hurdleEvents.js
@@ -0,0 +1,8 @@
+boys110.appendChild(athleteCheckbox(hurdlers.boys, boys110List, 'high'));
+boys300.appendChild(athleteCheckbox(hurdlers.boys, boys300List, 'low'));
+girls100.appendChild(athleteCheckbox(hurdlers.girls, girls100List, 'high'));
+girls300.appendChild(athleteCheckbox(hurdlers.girls, girls300List, 'low'));
+boysLj.appendChild(athleteCheckbox(jumpers.boys, boysLjList, 'long'));
+boysTj.appendChild(athleteCheckbox(jumpers.boys, boysTjList, 'triple'));
+girlsLj.appendChild(athleteCheckbox(jumpers.girls, girlsLjList, 'long'));
+girlsTj.appendChild(athleteCheckbox(jumpers.girls, girlsTjList, 'triple'));
