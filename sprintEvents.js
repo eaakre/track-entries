62e43944d4b2e27f093dd3fc7c7fd75dee3bf908
eaakre <58789308+eaakre@users.noreply.github.com>
2023-05-11 12:47:34 -0500

    sprints

--- sprintEvents.js
+++ sprintEvents.js
@@ -1,6 +1,6 @@
 boys100.appendChild(athleteCheckbox(sprinters.boys, boys100List, '100'));
-boys200.appendChild(athleteCheckbox(sprinters.boys, boys100List, '200'));
-boys400s.appendChild(athleteCheckbox(sprinters.boys, boys100List, '400'));
-boys4x1.appendChild(athleteCheckbox(sprinters.boys, boys100List, '4100'));
-boys4x2.appendChild(athleteCheckbox(sprinters.boys, boys100List, '4200'));
-boys4x4s.appendChild(athleteCheckbox(sprinters.boys, boys100List, '4400'));
+boys200.appendChild(athleteCheckbox(sprinters.boys, boys200List, '200'));
+boys400s.appendChild(athleteCheckbox(sprinters.boys, boys400sList, '400'));
+boys4x1.appendChild(athleteCheckbox(sprinters.boys, boys4100List, '4100'));
+boys4x2.appendChild(athleteCheckbox(sprinters.boys, boys4200List, '4200'));
+boys4x4s.appendChild(athleteCheckbox(sprinters.boys, boys4400sList, '4400'));
